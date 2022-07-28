43153dbd68c953838d6e91830fe43f575b9d78eb
kamini04 <kamini105btit20@igdtuw.ac.in>
2022-07-28 21:17:21 +0530

    chnges

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ polarity_score=[]
 for i in range(0, df.shape[0]):
     score=TextBlob(df.iloc[i][0])
     score1= score.sentiment[0]
-    polarity_score.appned(score1)
+    polarity_score.append(score1)
 
 df=pd.concat([df, pd.Series(polarity_score)], axis=1)
 df.rename(columns={df.colums[1]: "sentiment"}, inplace= True)
