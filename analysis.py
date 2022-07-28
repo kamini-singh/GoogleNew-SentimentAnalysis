219a921c0330d7108da8899c129447298f9e9e7c
kamini04 <kamini105btit20@igdtuw.ac.in>
2022-07-28 21:55:28 +0530

    changes made to analysis.py

--- analysis.py
+++ analysis.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from textblob import TextBlob
-df=pd.read_csv("url")
+df=pd.read_csv("Google_News.csv")
 polarity_score=[]
 
 for i in range(0, df.shape[0]):
     score=TextBlob(df.iloc[i][0])
-    score1= score.sentiment[0]
-    polarity_score.append(score1)
+    sentiment_score= score.sentiment[0]
+    polarity_score.append(sentiment_score)
 
 df=pd.concat([df, pd.Series(polarity_score)], axis=1)
-df.rename(columns={df.colums[1]: "sentiment"}, inplace= True)
-total=len(df[df.sentiment])
-positive=len(df[df.sentiment>.1]) / total
-negative=len(df[df.sentiment<0])/ total
-
+df.rename(columns={df.columns[1]: "Sentiment"}, inplace= True)
+total=len(df.Sentiment)
+positive=(len(df[df.Sentiment>0]) / total) *100
+negative=(len(df[df.Sentiment<0]) / total) *100
+neutral= 100- (positive+negative)
+print(df.head())
+print(total)
+print(positive)
+print(negative)
+print(neutral)
